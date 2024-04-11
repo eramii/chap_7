2f384211fb8a555bc115300ec52723d76b852624
eramii <yeramiya7@gmail.com>
2024-04-11 11:16:06 +0900

    Modify div function using exception

--- ctrl.py
+++ ctrl.py
@@ -19,8 +19,8 @@ class Control:
         self.view.btn2.clicked.connect(self.view.clearMessage)
         
     def sum(self, a, b):
-            return a+b
-    
+        return a+b
+        
     def sub(self, a, b):
         return a-b
     
@@ -39,3 +39,4 @@ class Control:
     
     def pow(self, a, b):
         return pow(a, b)
+    
